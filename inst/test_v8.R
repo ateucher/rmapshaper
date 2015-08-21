aa1923d463505c6b58a40e51dde710503b944ddb
ateucher <andy.teucher@gmail.com>
2015-08-20 20:13:03 -0700

    Trying mapshaper.js w/o browserify

--- inst/test_v8.R
+++ inst/test_v8.R
@@ -1,40 +1,23 @@
-## To get the mapshaper library ready:
-##
-## npm install mapshaper
-## npm install -g browserify
-## echo "global.mapshaper = require('mapshaper');" > in.js
-## browserify in.js -o mapshaper-bundle.js
-## put mapshaper-bundle.js in /inst/mapshaper
-
 library(V8)
 library(geojsonio)
-library(bcmaps)
 
-mapshaper <- "inst/mapshaper/mapshaper-bundle.js"
+mapshaper <- "mapshaper/mapshaper.js"
 ms <- new_context()
 ms$source(mapshaper)
 # Get setTimeout
-ms$source("inst/timers/timers.js")
-# Get setImmediate
-ms$source("inst/setImmediate/setImmediate.js")
+ms$source("timers/timers.js")
+# # Get setImmediate
+ms$source("setImmediate/setImmediate.js")
 
 callback <- "function(Error, data) {
-  if (Error) {
-console.log(Error);
-return;
-}
-var output = JSON.parse(data);
-return output;
+  if (Error) console.error(Error);
+  return data;
 }"
 
-bc_gj <- unclass(geojson_json(bc_bound))
+file <- system.file("examples", "california.geojson", package = "geojsonio")
+test_data <- as.json(geojson_read(file))
 
-test <- "-simplify 0.05 visvalingam"
+test_cmd <- "-simplify 0.05 visvalingam"
 
-## Closest to working here:
-foo <- ms$call("mapshaper.applyCommands", test, bc_gj, callback)
-
-ms$call(sprintf("var foo = mapshaper.runCommands(%s, '-i -simplify 5%% visvalingam keep-shapes'", bc_gj))
-
-ms$assign('bound', bc_gj)
-ms$eval(paste0("var foo = mapshaper.runCommands('", bc_gj, " -simplify 5% visvalingam keep-shapes', ", callback,")"))
+ms$assign("cb", JS(callback))
+ms$call("mapshaper.applyCommands", test_cmd, test_data, "cb")
