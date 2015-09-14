ca5a89a4fb2e0cc75e0f691f42c76ce3e3f142a1
Andy Teucher <andy.teucher@gmail.com>
2015-09-14 12:38:04 -0700

    test newly fixed applyCommands

--- inst/test_v8.R
+++ inst/test_v8.R
@@ -4,52 +4,59 @@ library(geojsonio)
 mapshaper <- "inst/mapshaper/mapshaper.js"
 ms <- new_context()
 ms$source(mapshaper)
-# Get setTimeout
-ms$source("inst/timers/timers.js")
-# # Get setImmediate
-# ms$source("inst/setImmediate/setImmediate.js")
+
+geojson_file <- system.file("examples", "california.geojson", package = "geojsonio")
+california <- geojson_read(geojson_file)
+topojson_file <- system.file("examples", "us_states.topojson", package = "geojsonio")
+us_states <- geojson_json(topojson_read(topojson_file))
+
+ms$eval("var return_data;")
 
 callback <- "function(Error, data) {
   if (Error) console.error(Error);
-  console.log(data);
+  return_data = data;
 }"
 
-poly <- '{
-  "type": "FeatureCollection",
-"features": [
-{
-  "type": "Feature",
-  "geometry": {
-  "type": "Polygon",
-  "coordinates": [
-  [
-  [-114.345703125, 39.4369879],
-  [-116.4534998, 37.18979823791],
-  [-118.4534998, 38.17698709],
-  [-115.345703125, 43.576878],
-  [-106.611328125, 43.4529188935547],
-  [-105.092834092, 46.20938402],
-  [-106.66859, 39.4389646],
-  [-103.6117867, 36.436756],
-  [-114.34579879, 39.4361929]
-  ]
-  ]
-  },
-  "properties": {"id": "foobar"}
-}
-]
-}'
+# simplify
+ms$call("mapshaper.applyCommands", "-simplify 0.05 visvalingam", california, JS(callback))
+ms$get("return_data")
 
-# file <- system.file("examples", "california.geojson", package = "geojsonio")
-# test_data <- as.json(geojson_read(file))
-# test_data <- paste0(readLines(file), collapse = "")
-test_cmd <- "-simplify 0.4 visvalingam"
+# simplify with snapping
+ms$call("mapshaper.applyCommands", "snap -simplify 0.05", california, JS(callback))
+ms$get("return_data")
 
-ms$assign("cb", JS(callback))
-ms$call("mapshaper.applyCommands", test_cmd, poly, JS(callback)) # returns NULL
+# convert to points (centroid)
+ms$call("mapshaper.applyCommands", "-points", california, JS(callback))
+ms$get("return_data")
+
+# explode then get centroids of all parts
+ms$call("mapshaper.applyCommands", "-explode -points", california, JS(callback))
+ms$get("return_data")
+
+# rename fields then simplify
+ms$call("mapshaper.applyCommands", "snap -rename-fields abbr=abbreviation -simplify 0.05", california, JS(callback))
+ms$get("return_data")
+
+# convert to lines
+ms$call("mapshaper.applyCommands", "-lines", california, JS(callback))
+ms$get("return_data")
+
+# simplify then convert to lines
+ms$call("mapshaper.applyCommands", "-simplify 0.05 -lines", california, JS(callback))
+ms$get("return_data")
+
+# explode then simplify, not allowing null geometries (i.e., keep all small
+# that would otherwise be simplified into non-existence)
+ms$call("mapshaper.applyCommands", "-explode -simplify 0.05 keep-shapes", california, JS(callback))
+ms$get("return_data")
+
+# explode then dissolve then simplify us states
+ms$call("mapshaper.applyCommands", "snap -explode -dissolve -simplify 0.05", us_states, JS(callback))
+ms$get("return_data")
 
 ## try in the V8 console
-ms$assign("poly", poly)
+ms$assign("poly", JS(poly))
+ms$assign("clip_poly", JS(clip_poly))
 ms$console() # javascript follows in the V8 console
 mapshaper.applyCommands("-simplify 0.4 visvalingam", poly, cb); // null
 typeof(cb) // function
@@ -57,3 +64,8 @@ typeof(poly) // string
 poly // looks ok??
 exit
 ## end of javascrpt
+
+mapshaper.applyCommands("-clip bbox=-117,37,-103,45", poly, function(Error, data) {
+  if (Error) console.error(Error);
+  console.log(data);
+})
