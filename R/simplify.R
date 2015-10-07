553f04c452be92083a14161ba82ef8c963bd8ba1
Andy Teucher <andy.teucher@gmail.com>
2015-10-07 16:09:09 -0700

    Examples for simplify

--- R/simplify.R
+++ R/simplify.R
@@ -22,6 +22,47 @@
 #'
 #' @return a simplified representation of the geometry in the same class as the input
 #' @export
+#'
+#' @examples
+#' # With a simple geojson object
+#' poly <- '{
+#'   "type": "Feature",
+#'   "properties": {},
+#'   "geometry": {
+#'     "type": "Polygon",
+#'     "coordinates": [[
+#'       [-70.603637, -33.399918],
+#'       [-70.614624, -33.395332],
+#'       [-70.639343, -33.392466],
+#'       [-70.659942, -33.394759],
+#'       [-70.683975, -33.404504],
+#'       [-70.697021, -33.419406],
+#'       [-70.701141, -33.434306],
+#'       [-70.700454, -33.446339],
+#'       [-70.694274, -33.458369],
+#'       [-70.682601, -33.465816],
+#'       [-70.668869, -33.472117],
+#'       [-70.646209, -33.473835],
+#'       [-70.624923, -33.472117],
+#'       [-70.609817, -33.468107],
+#'       [-70.595397, -33.458369],
+#'       [-70.587158, -33.442901],
+#'       [-70.587158, -33.426283],
+#'       [-70.590591, -33.414248],
+#'       [-70.594711, -33.406224],
+#'       [-70.603637, -33.399918]
+#'     ]]
+#'   }
+#' }'
+#' class(poly) <- "json"
+#'
+#' simplify(poly)
+#'
+#' # With a SpatialPolygonsDataFrame. You will need the rworldmap package for this example:
+#' library("rworldmap")
+#' world <- getMap()
+#' simplify(world)
+#'
 simplify <- function(sp_obj, keep = 0.05, method = NULL, keep_shapes = TRUE,
                      no_repair = FALSE, snap = TRUE) {
   UseMethod("simplify")
