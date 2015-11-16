a98e22c595ab02129fd6a986569efb6ed9a3a4a9
ateucher <andy.teucher@gmail.com>
2015-11-16 13:53:50 -0800

    Conditionally force FeatureClass in clip_erase

--- R/clip_erase.R
+++ R/clip_erase.R
@@ -2,40 +2,52 @@
 #'
 #' @param target the target layer. Can be json or sp class
 #' @param clip the clipping layer. Can be json or SpatialPolygonsDataFrame
+#' @param force_FC should the output be forced to be a FeatureCollection (or
+#'   Spatial*DataFrame) even if there are no attributes? Default \code{TRUE}.
+#'   FeatureCollections are more compatible with rgdal::readOGR and
+#'   geojsonio::geojson_sp. If FALSE and there are no attributes associated with
+#'   the geometries, a GeometryCollection (or Spatial object with no dataframe)
+#'   will be output.
 #'
 #' @return clipped target in the same class as the input target
 #' @export
-ms_clip <- function(target, clip) {
-  clip_erase(target, clip, type = "clip")
+ms_clip <- function(target, clip, force_FC = TRUE) {
+  clip_erase(target = target, clip = clip, type = "clip", force_FC = force_FC)
 }
 
-#' erase
+#'erase
 #'
-#' @param target the target layer. Can be json or sp class
-#' @param erase the erase layer. Can be json or SpatialPolygonsDataFrame
-#'
-#' @return erased target in the same format as the input target
-#' @export
-ms_erase <- function(target, erase) {
-  clip_erase(target, erase, type = "erase")
+#'@param target the target layer. Can be json or sp class
+#'@param erase the erase layer. Can be json or SpatialPolygonsDataFrame.
+#'@param force_FC should the output be forced to be a FeatureCollection (or
+#'  Spatial*DataFrame) even if there are no attributes? Default \code{TRUE}.
+#'  FeatureCollections are more compatible with rgdal::readOGR and
+#'  geojsonio::geojson_sp. If FALSE and there are no attributes associated with
+#'  the geometries, a GeometryCollection (or Spatial object with no dataframe)
+#'  will be output.
+#'@return erased target in the same format as the input target
+#'@export
+ms_erase <- function(target, erase, force_FC = TRUE) {
+  clip_erase(target = target, clip = erase, type = "erase", force_FC = force_FC)
 }
 
-clip_erase <- function(target, clip, type) {
+clip_erase <- function(target, clip, type, force_FC) {
+  if (!is.logical(force_FC)) stop("force_FC must be TRUE or FALSE")
   UseMethod("clip_erase")
 }
 
 #' @importFrom geojsonio lint
-clip_erase.json <- function(target, clip, type) {
-  if (!class(clip) == "json") stop("both target and clip must be json")
+clip_erase.json <- function(target, clip, type, force_FC) {
+  if (!is(clip, "json")) stop("both target and clip must be json")
   if (geojsonio::lint(target) != "valid" ||
       geojsonio::lint(clip) != "valid") {
     stop("both target and clip must be valid geojson objects")
   }
-  mapshaper_clip(target, clip, type = type)
+  mapshaper_clip(target = target, clip = clip, type = type, force_FC = force_FC)
 }
 
 #' @importFrom sp proj4string proj4string<- CRS spTransform identicalCRS
-clip_erase.SpatialPolygonsDataFrame <- function(target, clip, type) {
+clip_erase.SpatialPolygonsDataFrame <- function(target, clip, type, force_FC) {
   if (!is(target, "Spatial") || !is(clip, "Spatial")) stop("target and clip must be of class sp")
 
   target_proj <- proj4string(target)
@@ -49,14 +61,15 @@ clip_erase.SpatialPolygonsDataFrame <- function(target, clip, type) {
   target_geojson <- sp_to_GeoJSON(target)
   clip_geojson <- sp_to_GeoJSON(clip)
 
-  result <- mapshaper_clip(target_geojson, clip_geojson, type = type)
+  result <- mapshaper_clip(target = target_geojson, clip = clip_geojson,
+                           type = type, force_FC = force_FC)
 
   ret <- GeoJSON_to_sp(result, target_proj)
   ret
 }
 
 #' @importFrom V8 JS
-mapshaper_clip <- function(target_layer, clip_layer, type) {
+mapshaper_clip <- function(target_layer, clip_layer, type, force_FC) {
 
   ## Import the layers into the V8 session
   ms$eval(paste0('var target_geojson = ', target_layer))
@@ -97,22 +110,23 @@ mapshaper_clip <- function(target_layer, clip_layer, type) {
         }
         // Convert dataset to geojson for export
         // (or if other format supplied in output options)
-        return_data = mapshaper.internal.exportFileContent(data, outOpts);
+        return_data = mapshaper.internal.exportFileContent(data, outOpts)[0].content;
       })"
   )
 
-  add_id_cmd <- add_dummy_id_command()
-
-  ms$eval(paste0(
+  if (force_FC) {
+    add_id_cmd <- add_dummy_id_command()
+    ms$eval(paste0(
       "
       // make sure that a FeatureCollection is returned by applying a dummy id
       // to each geometry. Otherwise if there are no attributes (i.e., just
       // geometries, a GeometryCollection is output which readOGR doesn't like)
-      mapshaper.applyCommands(\"", add_id_cmd, "\", return_data[0].content, function(Error, data) {
+      mapshaper.applyCommands(\"", add_id_cmd, "\", return_data, function(Error, data) {
           if (Error) console.error(Error);
           return_data = data;
       })"
-  ))
+    ))
+  }
 
   out <- ms$get("return_data")
 
