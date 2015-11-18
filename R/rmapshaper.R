4eeeca428058bacf8f2af7a730f546a2318604a4
ateucher <andy.teucher@gmail.com>
2015-11-18 11:41:05 -0800

    Add package documentation

---
+++ R/rmapshaper.R
@@ -0,0 +1,19 @@
+#' rmapshaper: A package for editing spatial objects.
+#'
+#' Topologically-aware polygon simplification.
+#'
+#' @section rmapshaper functions:
+#'
+#' ms_simplify
+#'
+#' ms_explode
+#'
+#' ms_dissolve
+#'
+#' ms_clip
+#'
+#' ms_erase
+#'
+#' @docType package
+#' @name rmapshaper
+NULL
