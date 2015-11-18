2f37d7154de3be3d45601a15a000ead3182e290c
ateucher <andy.teucher@gmail.com>
2015-11-18 11:46:40 -0800

    Document ms_explode methods

--- R/explode.R
+++ R/explode.R
@@ -18,13 +18,26 @@ ms_explode <- function(input, force_FC = TRUE) {
   UseMethod("ms_explode")
 }
 
+#' @describeIn ms_explode Method for geo_json
 #' @export
 ms_explode.geo_json <- function(input, force_FC = TRUE) {
   apply_mapshaper_commands(data = input, command = "-explode", force_FC = force_FC)
 }
 
+#' @describeIn ms_explode Method for geo_list
+#' @export
+ms_explode.geo_list <- function(input, force_FC = TRUE) {
+  geojson <- geojsonio::geojson_json(input)
+
+  ret <- apply_mapshaper_commands(data = geojson, command = "-explode", force_FC = force_FC)
+
+  geojsonio::geojson_list(ret)
+}
+
 ## The method using mapshaper's explode works, but is waaaay slower than
 ## sp::disaggregate due to converstion to/from geojson
+
+#' @describeIn ms_explode Method for SpatialPolygonsDataFrame
 #' @export
 ms_explode.SpatialPolygonsDataFrame <- function(input, force_FC) {
   sp::disaggregate(input)
@@ -36,12 +49,3 @@ ms_explode.SpatialPolygonsDataFrame <- function(input, force_FC) {
 #
 #   GeoJSON_to_sp(ret, proj = attr(geojson, "proj4"))
 # }
-
-#' @export
-ms_explode.geo_list <- function(input, force_FC = TRUE) {
-  geojson <- geojsonio::geojson_json(input)
-
-  ret <- apply_mapshaper_commands(data = geojson, command = "-explode", force_FC = force_FC)
-
-  geojsonio::geojson_list(ret)
-}
