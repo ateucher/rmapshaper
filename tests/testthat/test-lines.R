573126bcfc7a5fb7ea8a13709079a2e4a04827f1
ateucher <andy.teucher@gmail.com>
2015-11-25 15:37:19 -0800

    More tests for lines

--- tests/testthat/test-lines.R
+++ tests/testthat/test-lines.R
@@ -26,6 +26,7 @@ poly_spdf <- geojson_sp(poly_geo_json)
 test_that("ms_lines works with all classes", {
   expected_json <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"TYPE\":1,\"rmapshaperid\":0},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[103,3],[103,2]]}},\n{\"type\":\"Feature\",\"properties\":{\"TYPE\":0,\"rmapshaperid\":1},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[103,2],[102,2],[102,3],[103,3]]}},\n{\"type\":\"Feature\",\"properties\":{\"TYPE\":0,\"rmapshaperid\":2},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[103,3],[104,3],[104,2],[103,2]]}},\n{\"type\":\"Feature\",\"properties\":{\"TYPE\":0,\"rmapshaperid\":3},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[102.5,1],[102.5,2],[103.5,2],[103.5,1],[102.5,1]]}}]}", class = c("json", "geo_json"))
 
+  expect_equal(ms_lines(unclass(poly_geo_json)), expected_json)
   expect_equal(ms_lines(poly_geo_json), expected_json)
   expect_equal(ms_lines(poly_geo_list), geojson_list(expected_json))
   expect_equal(ms_lines(poly_spdf), geojson_sp(expected_json))
@@ -43,4 +44,6 @@ test_that("ms_lines errors correctly", {
   expect_error(ms_lines("foo"), "Input is not valid geojson")
   expect_error(ms_lines(poly_geo_json, "bar"), "Unknown data field: bar")
   expect_error(ms_lines(poly_spdf, "bar"), "not all fields specified exist in input data")
+  expect_error(ms_lines(poly_geo_json, 1), "fields must be a character vector")
+  expect_error(ms_lines(poly_geo_json, force_FC = "true"), "force_FC must be TRUE or FALSE")
 })
