3472cbbb13f149800cc527c01752adf5fe581b40
ateucher <andy.teucher@gmail.com>
2015-11-25 21:39:20 -0800

    Finish points, add tests and documentation

--- R/points.R
+++ R/points.R
@@ -1,3 +1,30 @@
+#' Create points from a polygon layer
+#'
+#' Can be generated from the polygons by specifying \code{location} to be
+#' \code{"centroid"} or \code{"inner"}, OR by specifying fields in the
+#' attributes of the layer containing \code{x} and \code{y} coordinates.
+#'
+#' @param input input polygons object to convert to lines - can be a
+#'   \code{SpatialPolygonsDataFrame} or class \code{geo_json} or
+#'   \code{geo_list}
+#' @param location either \code{"centroid"} or \code{"inner"}. If
+#'   \code{"centroid"}, creates points at the centroid of the largest ring of
+#'   each polygon feature. if \code{"inner"}, creates points in the interior of
+#'   the largest ring of each polygon feature. Inner points are located away
+#'   from polygon boundaries. Must be \code{NULL} if \code{x} and \code{y} are
+#'   specified. If left as \code{NULL} (default), will use centroids.
+#' @param x name of field containing x coordinate values. Must be \code{NULL} if
+#'   \code{location} is specified.
+#' @param y name of field containing y coordinate values. Must be \code{NULL} if
+#'   \code{location} is specified.
+#' @param force_FC should the output be forced to be a \code{FeatureCollection}
+#'   even if there are no attributes? Default \code{TRUE}.
+#'   \code{FeatureCollections} are more compatible with \code{rgdal::readOGR}
+#'   and \code{geojsonio::geojson_sp}. If \code{FALSE} and there are no
+#'   attributes associated with the geometries, a \code{GeometryCollection} will
+#'   be output. Ignored for \code{Spatial} objects, as a
+#'   \code{SpatialPointsDataFrame} is always the output.
+#'
 #' @export
 ms_points <- function(input, location = NULL, x = NULL, y = NULL, force_FC = TRUE) {
   if (!is.logical(force_FC)) stop("force_FC must be TRUE or FALSE")
@@ -63,7 +90,7 @@ make_points_call <- function(location, x, y) {
   } else if (!is.null(x) && !is.null(y)) {
     call <- list("-points", paste0("x=", x), paste0("y=", y))
   } else {
-    stop("invalid call") ## should never get here
+    call <- "-points"
   }
 
   call
---
+++ tests/testthat/test-points.R
@@ -0,0 +1,54 @@
+context("ms_points")
+library(geojsonio)
+
+poly_geo_json <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-76.3,-49.68],[-75.53,-51.13],[-74.71,-56.89],[-84.11,-57.09],[-77.9,-50.62],[-84.12,-49.59],[-76.3,-49.68]]]},\"properties\":{\"x\": -78, \"y\": -53}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-68.77,69.82],[-66.26,62.96],[-74.22,60.87],[-74.12,65.22],[-74.55,65.81],[-75.66,67.03],[-68.77,69.82]]]},\"properties\":{\"x\": -71, \"y\": 65}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[136.27,65.8],[137.78,64.03],[140.03,59.56],[139.48,56.48],[133.64,62.44],[129.67,69.6],[136.27,65.8]]]},\"properties\":{\"x\": 135, \"y\": 65}}]}", class = c("json", "geo_json"))
+
+poly_geo_list <- geojson_list(poly_geo_json)
+poly_spdf <- geojson_sp(poly_geo_json)
+
+
+test_that("ms_points works with defaults", {
+  expected_out <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"x\":-78,\"y\":-53,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-78.4154562738861,-53.95000746272258]}},\n{\"type\":\"Feature\",\"properties\":{\"x\":-71,\"y\":65,\"rmapshaperid\":1},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-70.8687480648099,65.19505422895163]}},\n{\"type\":\"Feature\",\"properties\":{\"x\":135,\"y\":65,\"rmapshaperid\":2},\"geometry\":{\"type\":\"Point\",\"coordinates\":[135.65518268439885,63.10517782011297]}}]}", class = c("json", "geo_json"))
+
+  expect_equal(ms_points(poly_geo_json), expected_out)
+  expect_equal(ms_points(poly_geo_list), geojson_list(expected_out))
+  expect_equal(ms_points(poly_spdf), geojson_sp(expected_out))
+})
+
+test_that("ms_points works with location=centroid", {
+  expected_out <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"x\":-78,\"y\":-53,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-78.4154562738861,-53.95000746272258]}},\n{\"type\":\"Feature\",\"properties\":{\"x\":-71,\"y\":65,\"rmapshaperid\":1},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-70.8687480648099,65.19505422895163]}},\n{\"type\":\"Feature\",\"properties\":{\"x\":135,\"y\":65,\"rmapshaperid\":2},\"geometry\":{\"type\":\"Point\",\"coordinates\":[135.65518268439885,63.10517782011297]}}]}", class = c("json", "geo_json"))
+
+  expect_equal(ms_points(poly_geo_json, location = "centroid"), ms_points(poly_geo_json))
+  expect_equal(ms_points(poly_geo_json, location = "centroid"), expected_out)
+  expect_equal(ms_points(poly_geo_list, location = "centroid"), geojson_list(expected_out))
+  expect_equal(ms_points(poly_spdf, location = "centroid"), geojson_sp(expected_out))
+})
+
+test_that("ms_points works with location=inner", {
+  expected_out <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"x\":-78,\"y\":-53,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-77.94495627388609,-54.35054796472695]}},\n{\"type\":\"Feature\",\"properties\":{\"x\":-71,\"y\":65,\"rmapshaperid\":1},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-70.7792242552861,65.38990758263705]}},\n{\"type\":\"Feature\",\"properties\":{\"x\":135,\"y\":65,\"rmapshaperid\":2},\"geometry\":{\"type\":\"Point\",\"coordinates\":[135.73366753288371,63.20605469121952]}}]}", class = c("json", "geo_json"))
+
+  expect_equal(ms_points(poly_geo_json, location = "inner"), expected_out)
+  expect_equal(ms_points(poly_geo_list, location = "inner"), geojson_list(expected_out))
+  expect_equal(ms_points(poly_spdf, location = "inner"), geojson_sp(expected_out))
+})
+
+test_that("ms_points works with x and y", {
+  expected_out <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"x\":-78,\"y\":-53,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-78,-53]}},\n{\"type\":\"Feature\",\"properties\":{\"x\":-71,\"y\":65,\"rmapshaperid\":1},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-71,65]}},\n{\"type\":\"Feature\",\"properties\":{\"x\":135,\"y\":65,\"rmapshaperid\":2},\"geometry\":{\"type\":\"Point\",\"coordinates\":[135,65]}}]}", class = c("json", "geo_json"))
+
+  expect_equal(ms_points(poly_geo_json, x = "x", y = "y"), expected_out)
+  expect_equal(ms_points(poly_geo_list, x = "x", y = "y"), geojson_list(expected_out))
+  expect_equal(ms_points(poly_spdf, x = "x", y = "y"), geojson_sp(expected_out))
+})
+
+test_that("ms_points fails correctly", {
+  expect_error(ms_points(poly_geo_json, location = "inner", x = "x", y = "y"),
+               "You have specified both a location and x/y for point placement")
+  expect_error(ms_points(poly_geo_json, location = "inner", x = "x"),
+               "You have specified both a location and x/y for point placement")
+  expect_error(ms_points(poly_geo_json, location = "inner", y = "y"),
+               "You have specified both a location and x/y for point placement")
+  expect_error(ms_points(poly_geo_json, x = "x"), "Only one of x/y pair found")
+  expect_error(ms_points(poly_geo_json, y = "y"), "Only one of x/y pair found")
+  expect_error(ms_points(poly_geo_json, force_FC = "true"),
+               "force_FC must be TRUE or FALSE")
+})
