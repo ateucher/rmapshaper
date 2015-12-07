c11507f86ac09c857aaf4c10d16aaba73533a04e
ateucher <andy.teucher@gmail.com>
2015-12-06 21:55:24 -0800

    More tests

--- tests/testthat/test-filter_fields.R
+++ tests/testthat/test-filter_fields.R
@@ -24,5 +24,10 @@ class = c("json", "geo_json"))
 test_that("ms_filter_fields works with polygons", {
   expected_out <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"a\":1,\"b\":2},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[102,2],[102,4],[104,4],[104,2],[102,2]]]}}]}", class = c("json", "geo_json"))
   expect_equal(ms_filter_fields(poly, c("a", "b")), expected_out)
+  expect_equal(ms_filter_fields(geojson_list(poly), c("a", "b")), geojson_list(expected_out))
+  out_sp <- ms_filter_fields(geojson_sp(poly), c("a", "b"))
+  expect_is(out_sp, "SpatialPolygonsDataFrame")
+  expect_equal(out_sp@data, data.frame(a = 1, b = 2, row.names = 0L))
+
 })
 
