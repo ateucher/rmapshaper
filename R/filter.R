e6c007630e4cd23b6c775d0a73d669413c4fa183
ateucher <andy.teucher@gmail.com>
2016-01-16 07:51:58 -0800

    Resurrect ms_filter and tests from history (WTF?)

---
+++ R/filter.R
@@ -0,0 +1,154 @@
+#' Filter features based on attributes
+#'
+#' Apply a boolean expression to the attributes of each feature, removeing those
+#' which evaluate to \code{FALSE}
+#'
+#' @param input spatial object to filter - can be one of the \code{Spatial}
+#'   classes (e.g., \code{SpatialPolygonsDataFrame}) or class \code{geo_json}
+#' @param filter expression to apply. Can be a character vector of individual
+#'   conditional statements, which will be combined with logical \code{AND (&)},
+#'   or a single string combining expressions with \code{&} and/or \code{|}. You
+#'   can use javascript syntax if you are familiar with it, or R syntax which
+#'   will be converted to javascript internally.
+#' @param drop_null_geometries should features with empty geometries be dropped? Default \code{TRUE}
+#' @param force_FC should the output be forced to be a \code{FeatureCollection}
+#'   even if there are no attributes? Default \code{TRUE}.
+#'   \code{FeatureCollections} are more compatible with \code{rgdal::readOGR}
+#'   and \code{geojsonio::geojson_sp}. If \code{FALSE} and there are no
+#'   attributes associated with the geometries, a \code{GeometryCollection} will
+#'   be output. Ignored for \code{Spatial} objects, as a
+#'   \code{Spatial*DataFrame} is always the output.
+#'
+#' @return object with only specified features retained, in the same class as
+#'   the input
+#' @export
+ms_filter <- function(input, filter = NULL, drop_null_geometries = TRUE, force_FC = TRUE) {
+  if (!is.logical(force_FC)) stop("force_FC must be TRUE or FALSE")
+  UseMethod("ms_filter")
+}
+
+#' @describeIn ms_filter For character representations of geojson (for example
+#' if you used \code{readLines} to read in a geojson file)
+#' @export
+ms_filter.character <- function(input, filter = NULL, drop_null_geometries = TRUE, force_FC = TRUE) {
+  input <- check_character_input(input)
+
+  cmd <- make_filter_call(filter, drop_null_geometries)
+
+  apply_mapshaper_commands(data = input, command = cmd, force_FC = force_FC)
+
+}
+
+#' @describeIn ms_filter Method for geo_json
+#' @export
+ms_filter.geo_json <- function(input, filter = NULL, drop_null_geometries = TRUE, force_FC = TRUE) {
+  cmd <- make_filter_call(filter, drop_null_geometries)
+
+  apply_mapshaper_commands(data = input, command = cmd, force_FC = force_FC)
+}
+
+#' @describeIn ms_filter Method for geo_list
+#' @export
+ms_filter.geo_list <- function(input, filter = NULL, drop_null_geometries = TRUE, force_FC = TRUE) {
+  geojson <- geojsonio::geojson_json(input)
+
+  cmd <- make_filter_call(filter, drop_null_geometries)
+
+  ret <- apply_mapshaper_commands(data = geojson, command = cmd, force_FC = force_FC)
+
+  geojsonio::geojson_list(ret)
+}
+
+#' @describeIn ms_filter Method for SpatialPolygonsDataFrame
+#' @export
+ms_filter.SpatialPolygonsDataFrame <- function(input, filter = NULL, drop_null_geometries = TRUE, force_FC) {
+  ms_filter_sp(input = input, filter = filter, force_FC = force_FC)
+}
+
+#' @describeIn ms_filter Method for SpatialLinesDataFrame
+#' @export
+ms_filter.SpatialLinesDataFrame <- function(input, filter = NULL, drop_null_geometries = TRUE, force_FC) {
+  ms_filter_sp(input = input, filter = filter, force_FC = force_FC)
+}
+
+#' @describeIn ms_filter Method for SpatialPointsDataFrame
+#' @export
+ms_filter.SpatialPointsDataFrame <- function(input, filter = NULL, drop_null_geometries = TRUE, force_FC) {
+  ms_filter_sp(input = input, filter = filter, force_FC = force_FC)
+}
+
+ms_filter_sp <- function(input, filter = NULL, drop_null_geometries = TRUE, force_FC) {
+
+  cmd <- make_filter_call(filter, drop_null_geometries)
+
+  geojson <- sp_to_GeoJSON(input)
+
+  ret <- apply_mapshaper_commands(data = geojson, command = cmd, force_FC = TRUE)
+
+  GeoJSON_to_sp(ret, proj = attr(geojson, "proj4"))
+}
+
+make_filter_call <- function(filter, drop_null_geometries) {
+  # comparisons: ==, >, >=, <, <=, !
+  # http://www.w3schools.com/js/js_comparisons.asp
+  # types: is.na, is.numeric, is.integer, is.character - use typeof in javascript
+  # http://javascript.info/tutorial/type-detection
+
+  if (drop_null_geometries) rem <- "remove-empty" else rem <- NULL
+
+  filter <- make_js_expression(filter)
+
+  call <- list("-filter", rem, paste0("'", filter, "'"))
+  call
+}
+
+make_js_expression <- function(x) {
+  ## Convert any is.whatever calls to javscript equivalent
+  x <- switch_type_test(x)
+
+  ## Convert R-style single &,| to js-style &&,|| and ==/!= to ===/!==
+  x <- gsub("\\&+", "&&", x)
+  x <- gsub("\\|+", "||", x)
+  x <- gsub("==+", "===", x)
+  x <- gsub("!=+", "!==", x)
+  x <- gsub("\\bTRUE\\b", "true", x)
+  x <- gsub("\\bFALSE\\b", "false", x)
+
+  ## If more than one conditional statement, combine with &&
+  if (length(x) > 1) {
+    x <- paste0("(", x, ")")
+    x <- paste0(x, collapse = " && ")
+  }
+
+  message("JavaScript expression evaluated: '", x, "'")
+
+  x
+}
+
+switch_type_test <- function(x) {
+  type_lookup <- data.frame(
+    r_fn_re = c("(!)?is\\.na", "(!)?is\\.numeric", "(!)?is\\.character", "(!)?is\\.logical"),
+    js_type = c("\\1=== null", "\\1=== 'number'", "\\1=== 'string'", "\\1=== 'boolean'"),
+    js_fn_re = c("\\2", rep("typeof(\\2)", 3)),
+    stringsAsFactors = FALSE
+  )
+
+  # Make a list of logical vectors for whether or not the R function shows up in that element of x
+  types_rows <- lapply(x, function(y) {
+    vapply(type_lookup$r_fn_re, function(re) grepl(re, y), logical(1))
+  })
+
+  ret <- x
+  for (i in seq_along(ret)) {
+    types <- type_lookup[types_rows[[i]], ]
+    if (nrow(types) > 0) {
+      for (t in 1:nrow(types)) {
+        searchstr <- paste0(types[t,"r_fn_re"], "\\(([^)\r\n]+)\\)") ## Regex matches everything inside () except ) and line endings
+        ret[i] <- gsub(searchstr,
+                       paste(types[t,"js_fn_re"], types[t, "js_type"], sep = " "),
+                       ret[i])
+      }
+    }
+  }
+  ret
+}
---
+++ tests/testthat/test-filter.R
@@ -0,0 +1,78 @@
+context("ms_filter")
+library(geojsonio)
+
+poly_geo_json <- structure('{"type":"FeatureCollection",
+  "features":[
+ {"type":"Feature",
+ "properties":{"foo": null, "bar": 1, "baz": 10, "bat": true},
+ "geometry":{"type":"Polygon","coordinates":[[
+ [102,2],[102,3],[103,3],[103,2],[102,2]
+ ]]}}
+ ,{"type":"Feature",
+ "properties":{"foo": "b", "bar": 2, "baz": 20, "bat": true},
+ "geometry":{"type":"Polygon","coordinates":[[
+ [103,3],[104,3],[104,2],[103,2],[103,3]
+ ]]}},
+ {"type":"Feature",
+ "properties":{"foo": "c", "bar": 3, "baz": 30, "bat": false},
+ "geometry":{"type":"Polygon","coordinates":[[
+ [102.5,1],[102.5,2],[103.5,2],[103.5,1],[102.5,1]
+ ]]}}]}', class = c("json", "geo_json"))
+
+poly_geo_list <- geojson_list(poly_geo_json)
+
+poly_spdf <- geojson_sp(poly_geo_json)
+
+test_that("ms_filter works with all classes", {
+  expected_json <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"foo\":\"b\",\"bar\":2,\"baz\":20,\"bat\":true,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[103,3],[104,3],[104,2],[103,2],[103,3]]]}}]}", class = c("json", "geo_json"))
+
+  expect_equal(ms_filter(unclass(poly_geo_json), "foo == 'b'"), expected_json)
+  expect_equal(ms_filter(poly_geo_json, "foo == 'b'"), expected_json)
+  expect_equal(ms_filter(poly_geo_list, "foo == 'b'"), geojson_list(expected_json))
+  expect_equal(nrow(ms_filter(poly_spdf, "foo == 'b'")@data), 1)
+  expect_equal(ms_filter(poly_spdf, "foo == 'b'")@data$foo, factor("b"))
+  expect_equal(ms_filter(poly_spdf, "foo == 'b'")@polygons[[1]]@Polygons[[1]]@coords,
+               geojson_sp(expected_json)@polygons[[1]]@Polygons[[1]]@coords)
+})
+
+
+test_that("make_js_expression works", {
+  expect_equal(make_js_expression("is.na(foo)"), "foo === null")
+  expect_equal(make_js_expression("is.logical(foo)"), "typeof(foo) === 'boolean'")
+  expect_equal(make_js_expression("is.numeric(foo)"), "typeof(foo) === 'number'")
+  expect_equal(make_js_expression("is.character(foo)"), "typeof(foo) === 'string'")
+  expect_equal(make_js_expression("!is.na(foo)"), "foo !== null")
+  expect_equal(make_js_expression("!is.logical(foo)"), "typeof(foo) !== 'boolean'")
+  expect_equal(make_js_expression("!is.numeric(foo)"), "typeof(foo) !== 'number'")
+  expect_equal(make_js_expression("!is.character(foo)"), "typeof(foo) !== 'string'")
+  expect_equal(make_js_expression("foo == 'bar'"), "foo === 'bar'")
+  expect_equal(make_js_expression("foo != 'bar'"), "foo !== 'bar'")
+  expect_equal(make_js_expression(c("foo > 1", "bar == 2")), "(foo > 1) && (bar === 2)")
+  expect_equal(make_js_expression(c("!is.na(foo)", "bar == 2 | !is.logical(baz)")), "(foo !== null) && (bar === 2 || typeof(baz) !== 'boolean')")
+  expect_equal(make_js_expression("foo == TRUE | bar == FALSE"), "foo === true || bar === false")
+})
+
+test_that("lots of logical expressions work", {
+  expect_equal(ms_filter(poly_geo_json, "is.na(foo) | baz == 20"), structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"foo\":null,\"bar\":1,\"baz\":10,\"bat\":true,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[103,3],[103,2],[102,2],[102,3],[103,3]]]}},\n{\"type\":\"Feature\",\"properties\":{\"foo\":\"b\",\"bar\":2,\"baz\":20,\"bat\":true,\"rmapshaperid\":1},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[103,3],[104,3],[104,2],[103,2],[103,3]]]}}]}", class = c("json", "geo_json")))
+  expect_equal(ms_filter(poly_geo_json, c("bar > 1", "baz < 30")), structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"foo\":\"b\",\"bar\":2,\"baz\":20,\"bat\":true,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[103,3],[104,3],[104,2],[103,2],[103,3]]]}}]}", class = c("json", "geo_json")))
+  expect_equal(ms_filter(poly_geo_json, "bat == FALSE"), structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"foo\":\"c\",\"bar\":3,\"baz\":30,\"bat\":false,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[102.5,1],[102.5,2],[103.5,2],[103.5,1],[102.5,1]]]}}]}", class = c("json", "geo_json")))
+  expect_equal(ms_filter(poly_geo_json, c("baz > 20 | is.na(foo)", "bat == TRUE")), structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"foo\":null,\"bar\":1,\"baz\":10,\"bat\":true,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[103,3],[103,2],[102,2],[102,3],[103,3]]]}}]}", class = c("json", "geo_json")))
+  expect_equal(ms_filter(poly_geo_json, "bar + baz == 22"), structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"foo\":\"b\",\"bar\":2,\"baz\":20,\"bat\":true,\"rmapshaperid\":0},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[103,3],[104,3],[104,2],[103,2],[103,3]]]}}]}", class = c("json", "geo_json")))
+})
+
+
+# test_that("ms_lines works with fields specified", {
+#   expected_json <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"TYPE\":2,\"rmapshaperid\":0},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[103,3],[103,2]]}},\n{\"type\":\"Feature\",\"properties\":{\"TYPE\":0,\"rmapshaperid\":1},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[103,2],[102,2],[102,3],[103,3]]}},\n{\"type\":\"Feature\",\"properties\":{\"TYPE\":0,\"rmapshaperid\":2},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[103,3],[104,3],[104,2],[103,2]]}},\n{\"type\":\"Feature\",\"properties\":{\"TYPE\":0,\"rmapshaperid\":3},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[102.5,1],[102.5,2],[103.5,2],[103.5,1],[102.5,1]]}}]}", class = c("json", "geo_json"))
+#
+#   expect_equal(ms_lines(poly_geo_json, "foo"), expected_json)
+#   expect_equal(ms_lines(poly_geo_list, "foo"), geojson_list(expected_json))
+#   expect_equal(ms_lines(poly_spdf, "foo"), geojson_sp(expected_json))
+# })
+#
+# test_that("ms_lines errors correctly", {
+#   expect_error(ms_lines("foo"), "Input is not valid geojson")
+#   expect_error(ms_lines(poly_geo_json, "bar"), "Unknown data field: bar")
+#   expect_error(ms_lines(poly_spdf, "bar"), "not all fields specified exist in input data")
+#   expect_error(ms_lines(poly_geo_json, 1), "fields must be a character vector")
+#   expect_error(ms_lines(poly_geo_json, force_FC = "true"), "force_FC must be TRUE or FALSE")
+# })
