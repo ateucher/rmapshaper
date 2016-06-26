c59293e7911af538e3e0d0a9e530994c776704cd
ateucher <andy.teucher@gmail.com>
2016-06-25 22:40:58 -0700

    Add examples for ms_dissolve

--- R/dissolve.R
+++ R/dissolve.R
@@ -23,6 +23,39 @@
 #'  objects, as the output is always the same class as the input.
 #'
 #' @return the same class as the input
+#'
+#' @examples
+#' library(geojsonio)
+#' library(sp)
+#' 
+#' poly <- structure('{"type":"FeatureCollection",
+#'   "features":[
+#'   {"type":"Feature",
+#'   "properties":{"a": 1, "b": 2},
+#'   "geometry":{"type":"Polygon","coordinates":[[
+#'   [102,2],[102,3],[103,3],[103,2],[102,2]
+#'   ]]}}
+#'   ,{"type":"Feature",
+#'   "properties":{"a": 5, "b": 3},
+#'   "geometry":{"type":"Polygon","coordinates":[[
+#'   [100,0],[100,1],[101,1],[101,0],[100,0]
+#'   ]]}}]}', class = c("json", "geo_json"))
+#' poly <- geojson_sp(poly)
+#' plot(poly)
+#' length(poly)
+#' poly@data
+#' 
+#' # Dissolve the polygon
+#' out <- ms_dissolve(poly)
+#' plot(out)
+#' length(out)
+#' out@data
+#' 
+#' # Dissolve and summing columns
+#' out <- ms_dissolve(poly, sum_fields = c("a", "b"))
+#' plot(out)
+#' out@data
+#'
 #' @export
 ms_dissolve <- function(input, field = NULL, sum_fields = NULL, copy_fields = NULL, snap = TRUE, force_FC = TRUE) {
   UseMethod("ms_dissolve")
