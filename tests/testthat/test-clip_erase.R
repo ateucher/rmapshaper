ab8ec4e1bab06d87e0adcc418f53bc76b7e534c6
ateucher <andy.teucher@gmail.com>
2017-05-27 13:45:08 -0700

    Simple tests for sf classes for clip and erase

--- tests/testthat/test-clip_erase.R
+++ tests/testthat/test-clip_erase.R
@@ -1,5 +1,6 @@
 context("ms_clip_erase")
 library(geojsonlint)
+library(sf)
 suppressPackageStartupMessages(library(geojsonio, quietly = TRUE))
 
 poly <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[52.8658,-44.7219],[53.7702,-40.4873],[55.3204,-37.5579],[56.2757,-37.917],[56.184,-40.6443],[61.0835,-40.7529],[58.0202,-43.634],[61.6699,-45.0678],[62.737,-46.2841],[55.7763,-46.2637],[54.9742,-49.1184],[52.799,-45.9386],[52.0329,-49.5677],[50.1747,-52.1814],[49.0098,-52.3641],[52.7068,-45.7639],[43.2278,-47.1908],[48.4755,-45.1388],[50.327,-43.5207],[48.0804,-41.2784],[49.6307,-40.6159],[52.8658,-44.7219]]]}}]}", class = c("json", "geo_json"))
@@ -160,3 +161,29 @@ test_that("bbox works", {
   expect_error(ms_clip(poly, bbox = c(1,2,3)), "bbox must be a numeric vector of length 4")
   expect_error(ms_clip(poly, bbox = c("a","b","c", "d")), "bbox must be a numeric vector of length 4")
 })
+
+## test sf classes
+
+poly_sf <- st_as_sf(poly_spdf)
+poly_sfc <- st_as_sfc(poly_sp)
+lines_sf <- st_as_sf(line_spdf)
+points_sf <- st_as_sf(points_spdf)
+
+clip_sf <- read_sf(clip_poly)
+
+test_that("clip works with sf objects", {
+  expect_is(ms_clip(poly_sf, clip_sf), "sf")
+  expect_is(ms_clip(poly_sfc, clip_sf), "sfc")
+  expect_is(ms_clip(lines_sf, clip_sf), "sf")
+  expect_is(ms_clip(points_sf, clip_sf), "sf")
+  expect_is(ms_clip(poly_sf, bbox = c(51, -45, 55, -40)), "sf")
+})
+
+test_that("erase works with sf objects", {
+  expect_is(ms_erase(poly_sf, clip_sf), "sf")
+  expect_is(ms_erase(poly_sfc, clip_sf), "sfc")
+  expect_is(ms_erase(lines_sf, clip_sf), "sf")
+  expect_is(ms_erase(points_sf, clip_sf), "sf")
+  expect_is(ms_erase(poly_sf, bbox = c(51, -45, 55, -40)), "sf")
+})
+
