625b9d095385cde0731a26f236748e7b4ea335ca
ateucher <andy.teucher@gmail.com>
2017-05-27 15:02:01 -0700

    load pkgs queitly in testthat

--- tests/testthat/test-clip_erase.R
+++ tests/testthat/test-clip_erase.R
@@ -1,6 +1,6 @@
 context("ms_clip_erase")
-library(geojsonlint)
-library(sf)
+library(geojsonlint, quietly = TRUE)
+library(sf, quietly = TRUE)
 suppressPackageStartupMessages(library(geojsonio, quietly = TRUE))
 
 poly <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[52.8658,-44.7219],[53.7702,-40.4873],[55.3204,-37.5579],[56.2757,-37.917],[56.184,-40.6443],[61.0835,-40.7529],[58.0202,-43.634],[61.6699,-45.0678],[62.737,-46.2841],[55.7763,-46.2637],[54.9742,-49.1184],[52.799,-45.9386],[52.0329,-49.5677],[50.1747,-52.1814],[49.0098,-52.3641],[52.7068,-45.7639],[43.2278,-47.1908],[48.4755,-45.1388],[50.327,-43.5207],[48.0804,-41.2784],[49.6307,-40.6159],[52.8658,-44.7219]]]}}]}", class = c("json", "geo_json"))
