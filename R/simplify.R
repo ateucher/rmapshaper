94940d018248d2924380ea7e24a7d7ec4cdd4998
Robin Lovelace <rob00x@gmail.com>
2022-02-13 11:50:05 +0000

    Add browser for debugging for #75

--- R/simplify.R
+++ R/simplify.R
@@ -157,6 +157,7 @@ ms_simplify.sf <- function(input, keep = 0.05, method = NULL, weighting = 0.7,
                            force_FC = TRUE, drop_null_geometries = TRUE,
                            snap_interval = NULL, sys = FALSE, sys_mem = 8) {
 
+  browser()
   if (!all(sf::st_geometry_type(input) %in%
            c("LINESTRING", "MULTILINESTRING", "POLYGON", "MULTIPOLYGON"))) {
     stop("ms_simplify can only operate on (multi)polygons and (multi)linestrings",
