2638d7b3907f73fc5a691aed14d0c07bb39a4215
Andy Teucher <andy.teucher@gov.bc.ca>
2022-02-28 12:07:42 -0800

    Add test for #91

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -8,7 +8,9 @@ test_df <- data.frame(char = letters[1:3], dbl = c(1.1, 2.2, 3.3),
                       posix_ct = as.POSIXct(c("2016-01-25 11:25:03",
                                               "2017-01-27 23:24:56",
                                               "1979-03-09 10:25:15")),
-                      stringsAsFactors = FALSE)
+                      "column 6" = 1:3,
+                      stringsAsFactors = FALSE,
+                      check.names = FALSE)
 
 test_that("Restore column works", {
   cls <- col_classes(test_df)
@@ -18,9 +20,10 @@ test_that("Restore column works", {
   back_in <- fromJSON(toJSON(test_df))
   expect_equal(unname(sapply(back_in, class)),
                c("character", "numeric", "integer", "character", "character",
-                 "character", "character"))
+                 "character", "character", "integer"))
   restored <- restore_classes(df = back_in, classes = cls)
   expect_equal(lapply(test_df, class), lapply(restored, class))
+  expect_equal(names(test_df), names(restored)) # retain special names, https://github.com/ateucher/rmapshaper/issues/91
   expect_equal(test_df, restored)
   test_df$posix_lt <- as.POSIXlt(test_df$posix_ct)
   expect_error(col_classes(test_df), "POSIXlt classes not supported")
@@ -35,18 +38,17 @@ test_that("Restore columns works with rmapshaperid column", {
 })
 
 test_that("dealing with encoding works", {
+  test <- "Provence-Alpes-Côte d'Azur"
+  Encoding(test) <- "UTF-8"
+
   pts <- "{\"type\":\"FeatureCollection\",\"features\":[\n{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[-78,-53]},\"properties\":{\"x\":\"Provence-Alpes-Côte d'Azur\"}}]}"
   Encoding(pts) <- "UTF-8"
   out <- GeoJSON_to_sp(pts)
   expect_is(out, "SpatialPointsDataFrame")
-  test <- "Provence-Alpes-Côte d'Azur"
-  Encoding(test) <- "UTF-8"
   expect_equal(out$x[1], test)
 
   out <- GeoJSON_to_sf(pts)
   expect_is(out, "sf")
-  test <- "Provence-Alpes-Côte d'Azur"
-  Encoding(test) <- "UTF-8"
   expect_equal(out$x[1], test)
 })
 
