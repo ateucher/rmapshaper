7f788cec4eda7279fb927c20dcda43e43170329e
Andy Teucher <andy.teucher@gmail.com>
2023-04-05 15:51:26 -0700

    Update tests for drop_null_geometries

---
+++ tests/testthat/_snaps/drop_null_geometries.md
@@ -0,0 +1,14 @@
+# drop_null_geometries_works
+
+    {
+      "type": "character",
+      "attributes": {
+        "class": {
+          "type": "character",
+          "attributes": {},
+          "value": ["geojson", "json"]
+        }
+      },
+      "value": ["{\"type\":\"FeatureCollection\", \"features\": [\n{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[1,1]},\"properties\":{\"x\":1,\"rmapshaperid\":0}}\n]}"]
+    }
+
--- tests/testthat/test-drop_null_geometries.R
+++ tests/testthat/test-drop_null_geometries.R
@@ -1,10 +1,14 @@
-context("drop_null_geometries")
-suppressPackageStartupMessages(library("geojsonio"))
-
-poly_geojson <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-76.3,-49.68],[-75.53,-51.13],[-74.71,-56.89],[-84.11,-57.09],[-77.9,-50.62],[-84.12,-49.59],[-76.3,-49.68]]]},\"properties\":{\"x\":-78,\"y\":-53}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-68.77,69.82],[-66.26,62.96],[-74.22,60.87],[-74.12,65.22],[-74.55,65.81],[-75.66,67.03],[-68.77,69.82]]]},\"properties\":{\"x\":-71,\"y\":65}},{\"type\":\"Feature\",\"geometry\":{},\"properties\":{\"x\":135,\"y\":65}}]}", class = c("geojson", "json"))
-
 test_that("drop_null_geometries_works", {
-  expected_out <- structure("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-76.3,-49.68],[-75.53,-51.13],[-74.71,-56.89],[-84.11,-57.09],[-77.9,-50.62],[-84.12,-49.59],[-76.3,-49.68]]]},\"properties\":{\"x\":-78,\"y\":-53,\"rmapshaperid\":0}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-68.77,69.82],[-66.26,62.96],[-74.22,60.87],[-74.12,65.22],[-74.55,65.81],[-75.66,67.03],[-68.77,69.82]]]},\"properties\":{\"x\":-71,\"y\":65,\"rmapshaperid\":1}}]}", class = c("json","geojson"))
-
-  expect_equivalent_json(drop_null_geometries(poly_geojson), expected_out)
+poly_geojson <-   structure(
+  '{"type":"FeatureCollection",
+    "features":[{
+    "type":"Feature",
+    "geometry":{"type":"Point","coordinates":[1,1]},
+    "properties":{"x":1}},{
+    "type":"Feature",
+    "geometry":{},
+    "properties":{"x":2}}]}',
+  class = c("geojson", "json")
+)
+expect_snapshot_value(drop_null_geometries(poly_geojson), style = "json2")
 })
