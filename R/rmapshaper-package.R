02e814dafddf522bdc31c22b2ac87ab898fdb2f5
Andy Teucher <andy.teucher@gmail.com>
2023-04-21 22:53:28 -0700

    Fix package doc

--- R/rmapshaper-package.R
+++ R/rmapshaper-package.R
@@ -2,5 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom methods is .hasSlot as slot
+#' @importFrom V8 v8
+#' @importClassesFrom sp SpatialPoints SpatialLines
 ## usethis namespace: end
 NULL
